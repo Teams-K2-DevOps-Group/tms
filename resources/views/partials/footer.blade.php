23d94d36f531c2027c67f9f895901603cb986b3c
Godstime Onyibe <godstimeonyibe@gmail.com>
2025-04-01 19:55:00 +0100

    Designed the prototype of contact page and added its route in the nav link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,13 +7,13 @@
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+                    <a href="{{route('index')}}" class="hover:underline me-4 md:me-6">Home</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    <a href="{{route('about')}}" class="hover:underline me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Contact</a>
+                    <a href="{{route('contact')}}" class="hover:underline me-4 md:me-6">Contact</a>
                 </li>
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">Sign Up</a>
