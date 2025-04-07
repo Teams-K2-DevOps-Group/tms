287e48357209e15b38f772cf2242ea6d623d52a0
Fritexhub <fridayegedenwekori@gmail.com>
2025-04-07 11:09:11 +0100

    Edited git conflict.

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,11 @@ use Illuminate\Support\Facades\Route;
 Route::view('/','index')->name('index');
 Route::view('about','about')->name('about');
 Route::view('contact','contact')->name('contact');
-<<<<<<< HEAD
+
 Route::view('registration', 'registration')->name('registration');
 Route::view('dashboard', 'dashboard')->name('dashboard');
-=======
 
 Route::view('auth/registration', 'auth/registration')->name('registration');
 
 Route::view('auth/login','auth/login')->name('login');
 Route::view('forgotenpassword','forgotenpassword')->name('forgotenpassword');
-
->>>>>>> 991223b510d6c6512ace4f353b2f89313ad111b3
