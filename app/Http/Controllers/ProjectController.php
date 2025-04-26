7d7ec7458039d57a28a74d8ce6e0902703773b46
Godstime Onyibe <godstimeonyibe@gmail.com>
2025-04-26 12:24:38 +0100

    Implemented project creation

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{    
+    public function saveProject(Request $request)
+    {
+
+        
+        $user = Auth::user();
+        $request->validate([
+            'projectname' =>   ['required','string'],
+            'projectdesc' => ['required','string'],
+            'startdate' => ['required'],
+            'deadline' => ['required'],
+        ]);
+        $project = new Project;
+        $project->name = $request->projectname;
+        $project->description = $request->projectdesc;
+        $project->startdate = date('Y-m-d', strtotime($request->startdate));
+        $project->deadline = date('Y-m-d', strtotime($request->deadline));
+        $project->status = 'pending';
+        // $project->user_id = auth()->id();
+        $project->user_id = $user->id;
+        $project->slug = Str::slug($request->projectname) . '-' . Str::random(6);
+        $result = $project->save();
+        if ($result) {
+            return redirect()->route('dashboard')->with("message","You have successfully created project");
+        } else {
+            return back();
+        }
+
+    }
+}
